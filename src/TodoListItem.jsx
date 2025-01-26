ae9336762b6ca9acb9b95a0a280e256e03d7eee2
AidaBur <aidaburlutckaia@gmail.com>
2025-01-26 15:31:34 -0500

    Updated layout, styling, and fixed responsiveness

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { FaCheckCircle, FaTrash } from "react-icons/fa";
+import styles from "./TodoListItem.module.css";
 
 const TodoListItem = ({ todo, onRemoveTodo }) => {
   return (
-    <li>
+    <li className={styles.ListItem}>
+      <FaCheckCircle style={{ marginRight: "8px", color: "#3E89FF" }} />
       {todo.title}
-      <button type="button" onClick={() => onRemoveTodo(todo.id)}>
-        Remove
+      <button
+        type="button"
+        onClick={() => onRemoveTodo(todo.id)}
+        className={styles.removeButton}
+      >
+        <FaTrash />
       </button>
     </li>
   );
