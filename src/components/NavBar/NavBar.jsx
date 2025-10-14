09485dc649fadb588fe0c42abcce965c6b63f357
AidaBur <aidaburlutckaia@gmail.com>
2025-10-14 13:41:08 -0400

    Fix case-sensitive CSS module path for NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FaHome, FaList } from "react-icons/fa";
 
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 
 const Navbar = () => {
   return (
